f3c1cfdeb12f8581aeda507506c5c352551cece6
Abu Taher <abut70243@gmail.com>
2021-04-09 19:12:00 +0600

    after srearch result and stripe

--- index.js
+++ index.js
@@ -30,9 +30,10 @@ client.connect(err => {
     })
     //read
     app.get('/products', (req, res) => {
-        productCollection.find({})
+        const search = req.query.search;
+        productCollection.find({ name: { $regex: search}})
         .toArray((err, document) => {
-            res.send(document)
+                res.send(document)
         })
     })
     //read by key one product
